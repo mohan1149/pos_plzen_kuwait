bbd9ad897064c36803c27468d7cc32a3c91eba50
mohan1149 <mohan.velegacherla@gmail.com>
2022-12-13 09:42:35 +0300

    added rest

--- storage/framework/views/9390ad24939c03a518fd19fa28811980057fc158.php
+++
@@ -1,3 +0,0 @@
-<?php echo e($slot, false); ?>
-
-<?php /**PATH /Users/mohan/projects/mohan/laravel/cocoa/resources/views/vendor/mail/text/subcopy.blade.php ENDPATH**/ ?>
--- storage/framework/views/f105e2a2d5f16dccfbe0e8ede209c442710bdc56.php
+++
@@ -1,2 +0,0 @@
-[<?php echo e($slot, false); ?>](<?php echo e($url, false); ?>)
-<?php /**PATH /Users/mohan/projects/mohan/laravel/cocoa/resources/views/vendor/mail/text/header.blade.php ENDPATH**/ ?>
